3b4ebb4ebc260f2d1d87e97c7b5877b039f6c738
Jonathan Bruno <jonathanbruno.ueg@gmail.com>
2023-03-21 11:37:43 -0300

    type: :refactor
    allow custom content-language based on ebay's marketplace id

--- lib/ebay/connection_manager.rb
+++ lib/ebay/connection_manager.rb
@@ -7,6 +7,32 @@ module Ebay
       'Content-Language' => 'en-US'
     }.freeze
 
+    CONTENT_LANG_FOR_MARKETPLACE = {
+      EBAY_US: 'en-US',
+      EBAY_AT: 'de-AT',
+      EBAY_AU: 'en-AU',
+      EBAY_BE: 'nl-BE',
+      EBAY_CA: 'en-CA',
+      EBAY_CH: 'de-CH',
+      EBAY_DE: 'de-DE',
+      EBAY_ES: 'es-ES',
+      EBAY_FR: 'fr-FR',
+      EBAY_GB: 'en-GB',
+      EBAY_HK: 'zh-HK',
+      EBAY_IE: 'en-IE',
+      EBAY_IN: 'en-GB',
+      EBAY_IT: 'it-IT',
+      EBAY_MY: 'en-US',
+      EBAY_NL: 'nl-NL',
+      EBAY_PH: 'en-PH',
+      EBAY_PL: 'pl-PL',
+      EBAY_SG: 'en-US',
+      EBAY_TH: 'th-TH',
+      EBAY_TW: 'zh-TW',
+      EBAY_VN: 'en-US',
+      EBAY_MOTORS_US: 'en-US'
+    }
+
     def initialize(params)
       @params = params
     end
@@ -16,7 +42,9 @@ module Ebay
       ssl_options = config.ssl || {}
       Faraday.new(url: config.api_url, ssl: ssl_options) do |conn|
         conn.request :json
-        conn.headers = HEADERS
+        conn.headers = HEADERS.merge(
+          'Content-Language' => content_language_for(config.marketplace_id)
+        )
         conn.use Ebay::Middleware::Auth, config
         conn.use Ebay::Middleware::HttpException
         conn.use FaradayMiddleware::Gzip
@@ -32,6 +60,7 @@ module Ebay
         conn.headers = HEADERS.merge(
           'content-type' => 'application/x-www-form-urlencoded',
           'Authorization' => "Basic #{base64_auth}",
+          'Content-Language' => content_language_for(config.marketplace_id)
         )
         conn.use Ebay::Middleware::HttpException
         conn.adapter Faraday.default_adapter
@@ -57,7 +86,12 @@ module Ebay
           else
             "https://#{prefix}.ebay.com"
           end
+        config.marketplace_id = @params[:marketplace_id]
       end
+    end
+
+    def content_language_for(marketplace_id)
+      CONTENT_LANG_FOR_MARKETPLACE[marketplace_id] || 'en-US'
     end
   end
 end
--- lib/ebay/version.rb
+++ lib/ebay/version.rb
@@ -1,3 +1,3 @@
 module Ebay
-  VERSION = '1.0.7'.freeze
+  VERSION = '1.0.8'.freeze
 end
